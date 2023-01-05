7215e6a0ebdf0ec5cefad41391fb2d0e4296a108
Irving Zamora <irvingzamora28@gmail.com>
2023-01-05 00:07:43 -0700

    Add Navbar with logo

---
+++ components/Navbar.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div className="fixed w-full h-20 shadow-xl z-50">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image
+          src={"/../public/assets/images/logo-removebg.png"}
+          alt=""
+          width={125}
+          height={50}
+        />
+        <div>
+          <ul className="hidden md:flex">
+            <Link href={"/"}>
+              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+            </Link>
+            <Link href={"/"}>
+              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+            </Link>
+            <Link href={"/"}>
+              <li className="ml-10 text-sm uppercase hover:border-b">Skill</li>
+            </Link>
+            <Link href={"/"}>
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Projects
+              </li>
+            </Link>
+            <Link href={"/"}>
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Contact
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
