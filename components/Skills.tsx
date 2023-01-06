29ec78dd4afd738bb8d3e23cbebacc728cdbf4b0
Irving Zamora <irvingzamora28@gmail.com>
2023-01-06 02:35:49 -0600

    Add Skills component

---
+++ components/Skills.tsx
@@ -0,0 +1,177 @@
+import Image from "next/image";
+import React from "react";
+
+export const Skills = () => {
+    return (
+        <div className="w-full lg:h-screen p-2">
+            <div className="flex flex-col justify-center max-w-[1240px] mx-auto h-full">
+                <p className="text-xl tracking-widest text-sky-600">Skills</p>
+                <h2 className="py-4">What I can do</h2>
+                <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+                <div className="p-2 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+                        <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                            <div className="m-auto">
+                                <Image
+                                    src={
+                                        "/../public/assets/images/html5_logo.png"
+                                    }
+                                    alt="Irving Zamora"
+                                    width={128}
+                                    height={128}
+                                />
+                            </div>
+                            <div className="flex flex-col items-center justify-center text-gray-800 text-4xl md:text-xl">
+                                HTML
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="p-2 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+                        <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                            <div className="m-auto">
+                                <Image
+                                    src={
+                                        "/../public/assets/images/sass_logo.png"
+                                    }
+                                    alt="Irving Zamora"
+                                    width={128}
+                                    height={128}
+                                />
+                            </div>
+                            <div className="flex flex-col items-center justify-center text-gray-800 text-4xl md:text-xl">
+                                SASS
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="p-2 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+                        <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                            <div className="m-auto">
+                                <Image
+                                    src={
+                                        "/../public/assets/images/typescript_logo.png"
+                                    }
+                                    alt="Irving Zamora"
+                                    width={128}
+                                    height={128}
+                                />
+                            </div>
+                            <div className="flex flex-col items-center justify-center text-gray-800 text-4xl md:text-xl">
+                                TYPESCRIPT
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="p-2 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+                        <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                            <div className="m-auto">
+                                <Image
+                                    src={
+                                        "/../public/assets/images/nodejs_logo.png"
+                                    }
+                                    alt="Irving Zamora"
+                                    width={128}
+                                    height={128}
+                                />
+                            </div>
+                            <div className="flex flex-col items-center justify-center text-gray-800 text-4xl md:text-xl">
+                                NODEJS
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div className="p-2 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+                        <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                            <div className="m-auto">
+                                <Image
+                                    src={
+                                        "/../public/assets/images/vuejs_logo.png"
+                                    }
+                                    alt="Irving Zamora"
+                                    width={128}
+                                    height={128}
+                                />
+                            </div>
+                            <div className="flex flex-col items-center justify-center text-gray-800 text-4xl md:text-xl">
+                                VUEJS
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="p-2 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+                        <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                            <div className="m-auto">
+                                <Image
+                                    src={
+                                        "/../public/assets/images/laravel_logo.png"
+                                    }
+                                    alt="Irving Zamora"
+                                    width={128}
+                                    height={128}
+                                />
+                            </div>
+                            <div className="flex flex-col items-center justify-center text-gray-800 text-4xl md:text-xl">
+                                LARAVEL
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="p-2 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+                        <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                            <div className="m-auto">
+                                <Image
+                                    src={
+                                        "/../public/assets/images/mongodb_logo.png"
+                                    }
+                                    alt="Irving Zamora"
+                                    width={128}
+                                    height={128}
+                                />
+                            </div>
+                            <div className="flex flex-col items-center justify-center text-gray-800 text-4xl md:text-xl">
+                                MONGODB
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="p-2 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+                        <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                            <div className="m-auto">
+                                <Image
+                                    src={
+                                        "/../public/assets/images/git_logo.png"
+                                    }
+                                    alt="Irving Zamora"
+                                    width={128}
+                                    height={128}
+                                />
+                            </div>
+                            <div className="flex flex-col items-center justify-center text-gray-800 text-4xl md:text-xl">
+                                GIT
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="p-2 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+                        <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                            <div className="m-auto">
+                                <Image
+                                    src={
+                                        "/../public/assets/images/docker_logo.png"
+                                    }
+                                    alt="Irving Zamora"
+                                    width={128}
+                                    height={128}
+                                />
+                            </div>
+                            <div className="flex flex-col items-center justify-center text-gray-800 text-4xl md:text-xl">
+                                DOCKER
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { Inter } from "@next/font/google";
 import { Navbar } from "../components/Navbar";
 import Hero from "../components/Hero";
 import { About } from "../components/About";
+import { Skills } from "../components/Skills";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <About />
+      <Skills />
     </>
   );
 }
