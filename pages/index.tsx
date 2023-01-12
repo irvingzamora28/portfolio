cb73e5524fb3397965fa0c02dce4581270c3d7c0
Irving Zamora <irvingzamora28@gmail.com>
2023-01-12 05:39:41 -0600

    Remove package @next/font, it was causing an error because it needed tailwindcss. (tailwindcss is not available on production)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
 import Head from "next/head";
-import { Inter } from "@next/font/google";
-import { Navbar } from "../components/Navbar";
 import Hero from "../components/Hero";
 import { About } from "../components/About";
 import { Skills } from "../components/Skills";
 import { Projects } from "../components/Projects";
 import { Contact } from "../components/Contact";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
