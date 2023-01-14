33508f1ba60037f00b2f6d050187a5711bd21f55
Irving Zamora <irvingzamora28@gmail.com>
2023-01-14 01:46:10 -0600

    Add sass global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,9 @@
-import "../styles/globals.scss";
+import "../styles/globals.sass";
 import type { AppProps } from "next/app";
-import { Navbar } from "../components/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
-            <Navbar />
             <Component {...pageProps} />
         </>
     );
