6f5b4eb3f0108af1159d67c3af3fd70268c5ed66
Irving Zamora <irvingzamora28@gmail.com>
2023-02-15 08:11:16 -0600

    Fix lint warning: ./pages/index.tsx 60:8  Warning: React Hook useEffect has a missing dependency: 'handleScroll'. Either include it or remove the dependency array.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,10 @@ import Head from "next/head";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { AiOutlineFileText, AiOutlineHome, AiOutlineUser } from "react-icons/ai";
-import { BiImage, BiMoon, BiSun, BiUpArrowAlt} from "react-icons/bi"
+import { BiImage, BiMoon, BiSun, BiUpArrowAlt } from "react-icons/bi";
 import { FaBlog, FaTimes } from "react-icons/fa";
-import {MdOutlineContactMail} from "react-icons/md"
-import {TiThMenuOutline} from "react-icons/ti"
+import { MdOutlineContactMail } from "react-icons/md";
+import { TiThMenuOutline } from "react-icons/ti";
 import About from "../components/About";
 import Blogs from "../components/Blogs";
 import Contact from "../components/Contact";
@@ -15,63 +15,43 @@ import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 
 export default function Home() {
-    const [showMenu, setShowMenu] = useState(false)
-    const [showButton, setShowButton] = useState(false)
-    const [theme, setTheme] = useState("light")
-    const links = ["hero", "about", "skills", "blogs", "projects", "contact"];
+    const [showMenu, setShowMenu] = useState(false);
+    const [showButton, setShowButton] = useState(false);
+    const [theme, setTheme] = useState("light");
     const [activeLink, setActiveLink] = useState("hero");
-    const handleScroll = () => {
-        
-        if (window.pageYOffset > 250) {
-            setShowButton(true);
-        } else {
-            setShowButton(false);
-        }
-        
-        const currentSection = links.find(link => {
-            const section = document.getElementById(link);
-            return (
-                (section?.offsetTop ?? 0) <= window.pageYOffset + 50 &&
-                (section?.offsetTop ?? 0) + (section?.clientHeight ?? 0) > window.pageYOffset
-            );
-        });
-        setActiveLink(currentSection ? currentSection : "hero");
 
+    useEffect(() => {
+        const links = ["hero", "about", "skills", "blogs", "projects", "contact"];
+        function handleScroll() {
+            if (window.pageYOffset > 250) {
+                setShowButton(true);
+            } else {
+                setShowButton(false);
+            }
+            const currentSection = links.find((link) => {
+                const section = document.getElementById(link);
+                return (section?.offsetTop ?? 0) <= window.pageYOffset + 50 && (section?.offsetTop ?? 0) + (section?.clientHeight ?? 0) > window.pageYOffset;
+            });
+            setActiveLink(currentSection ? currentSection : "hero");
+        }
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    const toggleNav = () => {
+        setShowMenu(!showMenu);
     };
 
-    useEffect(() => {
-        function handleScroll() {
-          if (window.pageYOffset > 250) {
-            setShowButton(true);
-          } else {
-            setShowButton(false);
-          }
-        }
-    
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-      }, []);
-    
-    useEffect(() => {
-        window.addEventListener("scroll", handleScroll);
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
-    
-    const toggleNav = () => {
-        setShowMenu(!showMenu)
-    }
-    
     const handleTheme = () => {
-        setTheme(theme === "dark" ? "light" : "dark")    
-    }
+        setTheme(theme === "dark" ? "light" : "dark");
+    };
 
     return (
         <>
             <Head>
                 <title>Irving Zamora | Web Developer</title>
-                <meta charSet="utf-8"/>
+                <meta charSet="utf-8" />
                 <meta property="og:site_name" content="Irving Zamora | Web Developer" />
                 <meta property="og:title" content="Portfolio Irving Zamora" />
                 <meta property="og:description" content="As a web developer, I am passionate about building beautiful, fast, and responsive websites and web applications." />
@@ -79,11 +59,8 @@ export default function Home() {
                 <meta property="og:type" content="website" />
                 <meta name="description" content="As a web developer, I am passionate about building beautiful, fast, and responsive websites and web applications." />
                 <meta name="author" content="Irving Zamora" />
-                <meta name="keywords" content="irvingzamora, programming, nextjs, laravel, php, mysql, typescript"/>
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
+                <meta name="keywords" content="irvingzamora, programming, nextjs, laravel, php, mysql, typescript" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
@@ -99,80 +76,56 @@ export default function Home() {
                         Irving
                     </a>
                     <div className="nav__buttons">
-                        {theme === "dark" ? 
-                        <BiSun className="nav__toggle-darktheme" id="button-darktheme" onClick={handleTheme} /> : 
-                        <BiMoon className="nav__toggle-darktheme" id="button-darktheme" onClick={handleTheme} />
-                        }
-                      <div className="nav__toggle" id="nav-toggle" onClick={toggleNav}>
-                        <TiThMenuOutline /> 
-                      </div>
+                        {theme === "dark" ? (
+                            <BiSun className="nav__toggle-darktheme" id="button-darktheme" onClick={handleTheme} />
+                        ) : (
+                            <BiMoon className="nav__toggle-darktheme" id="button-darktheme" onClick={handleTheme} />
+                        )}
+                        <div className="nav__toggle" id="nav-toggle" onClick={toggleNav}>
+                            <TiThMenuOutline />
+                        </div>
                     </div>
-                    <div className={"nav__menu " + (showMenu? "show-menu" : "")} id="nav-menu">
+                    <div className={"nav__menu " + (showMenu ? "show-menu" : "")} id="nav-menu">
                         <ul className="nav__list">
                             <li className="nav__item">
-                                <a href={"/#"} className={`nav__link flex items-center ${
-                                activeLink === "hero"
-                                    ? "nav__link-active"
-                                    : ""
-                            }`} onClick={toggleNav}>
-                                <AiOutlineHome className="nav__icon" />
-                                <div className="pl-1">Home</div>
+                                <a href={"/#"} className={`nav__link flex items-center ${activeLink === "hero" ? "nav__link-active" : ""}`} onClick={toggleNav}>
+                                    <AiOutlineHome className="nav__icon" />
+                                    <div className="pl-1">Home</div>
                                 </a>
                             </li>
                             <li className="nav__item">
-                                <a href={"/#about"} className={`nav__link flex items-center ${
-                                activeLink === "about"
-                                    ? "nav__link-active"
-                                    : ""
-                            }`} onClick={toggleNav}>
-                                  <AiOutlineUser className="nav__icon" />
-                                  <div className="pl-1">About</div>
+                                <a href={"/#about"} className={`nav__link flex items-center ${activeLink === "about" ? "nav__link-active" : ""}`} onClick={toggleNav}>
+                                    <AiOutlineUser className="nav__icon" />
+                                    <div className="pl-1">About</div>
                                 </a>
                             </li>
                             <li className="nav__item">
-                                <a href={"/#skills"} className={`nav__link flex items-center ${
-                                activeLink === "skills"
-                                    ? "nav__link-active"
-                                    : ""
-                            }`} onClick={toggleNav}>
-                                  <AiOutlineFileText className="nav__icon" />
-                                  <div className="pl-1">Skills</div>
+                                <a href={"/#skills"} className={`nav__link flex items-center ${activeLink === "skills" ? "nav__link-active" : ""}`} onClick={toggleNav}>
+                                    <AiOutlineFileText className="nav__icon" />
+                                    <div className="pl-1">Skills</div>
                                 </a>
                             </li>
                             <li className="nav__item">
-                                <a href={"/#blogs"} className={`nav__link flex items-center ${
-                                activeLink === "blogs"
-                                    ? "nav__link-active"
-                                    : ""
-                            }`} onClick={toggleNav}>
-                                  <FaBlog className="nav__icon" />
-                                  <div className="pl-1">Blog</div>
+                                <a href={"/#blogs"} className={`nav__link flex items-center ${activeLink === "blogs" ? "nav__link-active" : ""}`} onClick={toggleNav}>
+                                    <FaBlog className="nav__icon" />
+                                    <div className="pl-1">Blog</div>
                                 </a>
                             </li>
                             <li className="nav__item">
-                                <a href={"/#projects"} className={`nav__link flex items-center ${
-                                activeLink === "projects"
-                                    ? "nav__link-active"
-                                    : ""
-                            }`} onClick={toggleNav}>
-                                  <BiImage className="nav__icon" />
-                                  <div className="pl-1">Projects</div>
+                                <a href={"/#projects"} className={`nav__link flex items-center ${activeLink === "projects" ? "nav__link-active" : ""}`} onClick={toggleNav}>
+                                    <BiImage className="nav__icon" />
+                                    <div className="pl-1">Projects</div>
                                 </a>
                             </li>
                             <li className="nav__item">
-                                <a href={"/#contact"} className={`nav__link flex items-center ${
-                                activeLink === "contact"
-                                    ? "nav__link-active"
-                                    : ""
-                            }`} onClick={toggleNav}>
-                                  <MdOutlineContactMail className="nav__icon" />
-                                  <div className="pl-1">Contact</div>
+                                <a href={"/#contact"} className={`nav__link flex items-center ${activeLink === "contact" ? "nav__link-active" : ""}`} onClick={toggleNav}>
+                                    <MdOutlineContactMail className="nav__icon" />
+                                    <div className="pl-1">Contact</div>
                                 </a>
                             </li>
                         </ul>
                         <FaTimes className="nav__close" id="nav-close" onClick={toggleNav} />
                     </div>
-
                 </nav>
             </header>
 
@@ -184,11 +137,10 @@ export default function Home() {
                 <Blogs />
                 <Contact />
                 <Footer />
-                <a href={"#"} className={`scrollup ${showButton ? 'scrollup-show' : ''} ${showMenu ? ' scrollup-show-menu' : ''}`} id="scroll-up">
+                <a href={"#"} className={`scrollup ${showButton ? "scrollup-show" : ""} ${showMenu ? " scrollup-show-menu" : ""}`} id="scroll-up">
                     <BiUpArrowAlt className="scrollup__icon" />
                 </a>
             </main>
-
         </>
     );
 }
