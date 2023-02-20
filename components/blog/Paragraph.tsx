ac4fdbe20d8173749d4672a9870724d42a37d6bd
Irving Zamora <irvingzamora28@gmail.com>
2023-02-19 19:07:52 -0600

    Add function to allow generating Links with the following structure ***text***/link

--- components/blog/Paragraph.tsx
+++ components/blog/Paragraph.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 interface Props {
@@ -6,43 +7,59 @@ interface Props {
 }
 
 const Paragraph: React.FC<Props> = (props) => {
-    // TODO: remove duplicate parseText function from Paragraph and ListItem components
-    const parseText = (text: string): any[] => {
-        const boldRegex = /\*{2}([^*]+)\*{2}/g;
-        let parts: any[] = []; // initialize an empty array to store parsed elements
-        let match;
-      
-        while ((match = boldRegex.exec(text)) !== null) {
-          const before = text.slice(0, match.index); // get the text before the bolded text
-          if (before.length > 0) { // check if there is any text before the bolded text
-            parts = [...parts, ...parseNewLines(before)]; // add any new lines to the parts array
-          }
-      
-          parts.push(React.createElement("b", { key: match.index }, match[1])); // add the bolded text to the parts array
-      
-          text = text.slice(match.index + match[0].length); // update the text to remove the bolded text
-        }
-      
-        if (text.length > 0) { // check if there is any remaining text
-          parts = [...parts, ...parseNewLines(text)]; // add any new lines to the parts array
-        }
-      
-        return parts; // return the final parts array
-      };
-      
-      const parseNewLines = (text: string): any[] => {
-        return text.split("\n").map((line, index) => {
-          return (
-            <React.Fragment key={index}>
-              {line}
-              <br />
-            </React.Fragment>
-          );
-        });
-      };
-      
+    // TODO: remove duplicate generateFormattedText function from Paragraph and ListItem components
+    const parseNewLines = (text: string): JSX.Element[] => {
+      return text.indexOf('\n') > 1 ? text.split('\n').map((line, index) => {
+        return (
+          <React.Fragment key={index}>
+            {line}
+            <br />
+          </React.Fragment>
+        );
+      }) : [<>{text}</>];
+    };
 
-    return React.createElement("p", { className: "py-2 " + props.class }, parseText(props.text));
+    const generateFormattedText = (text: string): JSX.Element[] => {
+      const boldRegex = /\*{2}([^*]+)\*{2}/g;
+      const linkRegex = /\*{3}([^*]+)\*{3}/g;
+      let parts: any[] = [];
+      let match;
+    
+      while ((match = boldRegex.exec(text)) !== null || (match = linkRegex.exec(text)) !== null) {
+        const before = text.slice(0, match.index);
+        if (before.length > 0) {
+          parts = [...parts, ...parseNewLines(before)];
+        }
+    
+        if (match[0].startsWith('***')) { // if it's a link
+          const hrefMatch = text.slice(match.index+1).match(/\*{3}([^ ]+)/);
+          if (!hrefMatch) {
+            throw new Error('Link text found but no link was provided');
+          }
+          const href = hrefMatch[1];
+    
+          parts.push(
+            <Link href={href} key={match.index} className={"text-blue-600"}>
+              {match[1]}
+            </Link>
+          );
+    
+          text = text.slice(match.index + match[0].length + href.length);
+        } else { // if it's bolded text
+          parts.push(React.createElement("b", { key: match.index }, match[1]));
+          text = text.slice(match.index + match[0].length);
+        }
+      }
+    
+      if (text.length > 0) {
+        parts = [...parts, ...parseNewLines(text)];
+      }
+    
+      return parts;
+    };
+    
+
+    return React.createElement("p", { className: "py-2 " + props.class }, generateFormattedText(props.text));
 };
 
 export default Paragraph;
