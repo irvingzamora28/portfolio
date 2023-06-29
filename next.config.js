c93c6ef1be7925d888ae5c3322937308d822451d
Irving Zamora <irvingzamora28@gmail.com>
2023-06-28 22:30:21 -0600

    Add configuration for mdx files and create hello world example

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   output: "standalone",
   images: {
     remotePatterns: [
@@ -18,4 +19,17 @@ const nextConfig = {
   },
 }
 
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    providerImportSource: '@mdx-js/react',
+  },
+});
+
 module.exports = nextConfig
+
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+});
