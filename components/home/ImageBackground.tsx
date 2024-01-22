2c6cb74e6d5b70c7b3bbbb1416304ec56e1a1952
Irving Zamora <irvingzamora28@gmail.com>
2024-01-22 04:39:44 -0600

    fix: Update image file and positioning in ImageBackground component

--- components/home/ImageBackground.tsx
+++ components/home/ImageBackground.tsx
@@ -3,10 +3,7 @@ import React from "react";
 const ImageBackground = () => {
     return (
         <div className="hero__blob">
-            <svg
-                viewBox="0 0 200 187"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlnsXlink="http://www.w3.org/1999/xlink">
+            <svg viewBox="0 0 200 187" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
                 <mask id="mask0" mask-type="alpha">
                     <path
                         d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 165.547 
@@ -22,7 +19,7 @@ const ImageBackground = () => {
         129.362C2.45775 97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 
         -0.149132 97.9666 0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"
                     />
-                    <image className="hero__blob-img" xlinkHref="/assets/images/profile.png" x="-40" y="-90" width="360" height="400" />
+                    <image className="hero__blob-img" xlinkHref="/assets/images/profile-picture.png" x="-60" y="-140" width="360" height="400" />
                 </g>
             </svg>
         </div>
